2a4f9f4f9e3b82f74799126ba2328a6b84449270
Alihan <alihanboysgar@gmail.com>
2022-11-30 01:14:40 +0300

    First commit

---
+++ configs/setup.go
@@ -0,0 +1,36 @@
+package configs
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func ConnectDB() *mongo.Client {
+	client, err := mongo.NewClient(options.Client().ApplyURI(EnvMongoURI()))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return client
+}
+
+var DB *mongo.Client = ConnectDB()
+
+func GetCollection(client *mongo.Client, x1 string) *mongo.Collection {
+	collection := client.Database("x1").Collection(x1)
+	return collection
+}
